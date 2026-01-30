86faf0e5d6694d6f529a69f26923a279804f874d
Anupama <you@example.comanuanupama41@gmail.com>
2026-01-31 00:39:00 +0530

    added allure-playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -27,10 +27,11 @@ export default defineConfig({
   //outputDir: 'test-results-report/',
 
   reporter: [
-    ['list'],
+    //['list'],
     ['html', { outputFolder: 'playwright-html-report' }],
-    ['junit', { outputFile: 'results.xml' }],
-    ['json', { outputFile: 'report.json' }],
+   // ['junit', { outputFile: 'results.xml' }],
+    //['json', { outputFile: 'report.json' }],
+    ['allure-playwright'],
     ['playwright-html-reporter', { 
       testFolder: 'tests',
       title: 'Playwright HTML Report',
